8f58db28fbbdb62b32ac718db9d673a13e112b0c
Jonathan Whiting <jonathan@ignika.com>
2013-09-19 22:13:04 +0100

    First succesfully emmitted instruction

--- src/assembler/assembler.c
+++ src/assembler/assembler.c
@@ -19,13 +19,74 @@ Test tests[NUM_TESTS] = {
 #define NUM_TOKENS (4)
 #define MAX_TOKEN_LENGTH (3)
 
-int create_instruction(unsigned char tokens[NUM_TOKENS][MAX_TOKEN_LENGTH+1], unsigned char *c)
+int get_arg(unsigned char token[MAX_TOKEN_LENGTH], int len, int line)
+{
+	int val=0;
+	int i;
+	for(i=0; i<MAX_TOKEN_LENGTH; i++)
+	{
+		if(token[i] == 0)
+			break;
+		int v = -1;
+		if(token[i] >= '0' && token[i] <= '9')
+			v = token[i]-'0';
+		if(token[i] >= 'a' && token[i] <= 'f')
+			v = token[i]-'a'+10;
+		if(v==-1)
+		{
+			printf("\nInvalid hex char '%c' (0x%02x) in line %d", token[i], token[i], line);
+			return -1;
+		}
+		if(i >= len)
+		{
+			printf("\nArg too long on %d", line);
+			return -1;
+		}
+		val = (val<<4)+v;
+	}
+	return val;
+}
+
+typedef enum
+{
+	ARGS_D,
+	ARGS_DV,
+	ARGS_AB,
+	ARGS_DAB,
+	ARGS_INVALID,
+} ArgsType;
+
+int create_instruction(unsigned char tokens[NUM_TOKENS][MAX_TOKEN_LENGTH], unsigned char *c, int line)
 {
 	(void)tokens;
 	(void)c;
 
+	ArgsType type = ARGS_INVALID;
 	if(strncmp((char*)tokens[0], "val", MAX_TOKEN_LENGTH)==0)
+	{
 		*c = 0x00;
+		type = ARGS_DV;
+	}	
+	int d;
+	int v;
+	// int a;
+	// int b;
+	switch(type)
+	{
+		case ARGS_DV:
+		{
+			d = get_arg(tokens[1], 1, line);
+			v = get_arg(tokens[2], 2, line);
+			if(d == -1 || v == -1)
+				return 1;
+			*c |= d;
+			*(c+1) = v;
+			break;
+		}
+		default:
+			printf("\nUnsupported arg type %d", type);
+			return 1;
+	}
 	return 0;
 }
 
@@ -37,7 +98,7 @@ int tokenize(const unsigned char *in, unsigned char *out, int *out_size)
 	int line = 1;
 	int token_num = 0;
 	int token_pos = 0;
-	unsigned char tokens[NUM_TOKENS][MAX_TOKEN_LENGTH+1];
+	unsigned char tokens[NUM_TOKENS][MAX_TOKEN_LENGTH];
 	memset(tokens, 0, sizeof(tokens));
 	for(in_off=0; in_off<TEST_PROGRAM_SIZE; in_off++)
 	{
@@ -64,7 +125,7 @@ int tokenize(const unsigned char *in, unsigned char *out, int *out_size)
 			}
 			if(token_num || token_pos)
 			{
-				int fail = create_instruction(tokens, &out[out_off]);
+				int fail = create_instruction(tokens, &out[out_off], line);
 				if(fail) return 1;
 				out_off+=2;
 			}
@@ -106,7 +167,7 @@ int tokenize(const unsigned char *in, unsigned char *out, int *out_size)
 		printf("\nUnrecognised char '%c' (0x%x) on line %d", c, c, line);
 		return 1;
 	}
-	*out_size = out_off-1;
+	*out_size = out_off;
 	return 0;
 }
 
@@ -122,10 +183,9 @@ int run_test(int n)
 		return 1;
 	}
 	int differ = 0;
-	unsigned char out_comparator[TEST_PROGRAM_SIZE];
 	int i=0;
 	for(i=0; i<tests[n].output_size; i++)
-		differ |= out[i] != out_comparator[i];
+		differ |= out[i] != tests[n].output[i];
 	if(differ)
 	{
 		printf("\nTEST %d FAILED: Output differed.", n);
